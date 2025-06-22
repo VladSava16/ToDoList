adee2c723ab3441f1b21939337ae2d1291e99bb9
Vlad Sava <savavlad82@gmail.com>
2025-06-22 17:20:31 +0300

    fixed positioning

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -34,14 +34,16 @@ const Todolist = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>Insert an item into the list</label>
-        <input type='text' className='outline m-8' value={inputValue} onChange={handleChange}></input>
-        <button type="submit" className='outline'>Insert</button>
-      </form>
-      <div>
-        <ul>
+    <div className='grid grid-cols-1'>
+      <div className='flex flex-col items-center justify-center'>
+        <form onSubmit={handleSubmit} className='flex items-center'>
+          <label>Insert an item into the list</label>
+          <input type='text' className='outline m-8' value={inputValue} onChange={handleChange}></input>
+          <button type="submit" className='outline'>Insert</button>
+        </form>
+      </div>
+      <div className='flex flex-col items-center justify-center w-full max-w-2xl mx-auto px-4'>
+        <ul className='w-full'>
           {items.map((item, index) => (
             <ToDoItem key={index} task={item} onRemove={handleRemove} onCompletion={handleComplete}/>
           ))}
