f60edc87995d0a00edff0845f6d981868de26466
Vlad Sava <savavlad82@gmail.com>
2025-06-27 16:56:00 +0300

    added search field element

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import ToDoItem from './ToDoItem';
 import ListHeader from './ListHeader';
 import InputHeader from './InputHeader';
+import SearchBar from './SearchBar';
 
 const Todolist = () => {
   
@@ -83,36 +84,33 @@ const Todolist = () => {
   if(items.length > 0){
     switch(filterBy){
       case 0: fullListRender = 
-        <div className={`flex flex-col items-center justify-center w-full max-w-2xl mx-auto px-4`}>
-          <ul className='max-w-xl w-full bg-gray-50 rounded-xl shadow-xl p-4'>
-            <ListHeader sortOrder={sortOrder} handleSortOrderChange={handleSortOrderChange} handleFilterBy={handleFilterBy} filterByStatus={filterBy}/>
-            {items.map((item, index) => (
-              <ToDoItem key={index} task={item} onRemove={handleRemove} onCompletion={handleComplete} onSubmitEdit={handleEdit}/>
-            ))}
-          </ul>
-        </div>   
+        <ul className='max-w-xl w-full bg-gray-50 rounded-xl shadow-xl p-4 flex flex-col gap-3'>
+          <SearchBar />
+          <ListHeader sortOrder={sortOrder} handleSortOrderChange={handleSortOrderChange} handleFilterBy={handleFilterBy} filterByStatus={filterBy}/>
+          {items.map((item, index) => (
+            <ToDoItem key={index} task={item} onRemove={handleRemove} onCompletion={handleComplete} onSubmitEdit={handleEdit}/>
+          ))}
+        </ul>
         break; 
       case 1: fullListRender = 
-        <div className={`flex flex-col items-center justify-center w-full max-w-2xl mx-auto px-4`}>
-          <ul className='max-w-xl w-full bg-gray-50 rounded-xl shadow-xl p-4'>
-            <ListHeader sortOrder={sortOrder} handleSortOrderChange={handleSortOrderChange} handleFilterBy={handleFilterBy} filterByStatus={filterBy}/>
-            {items.map((item, index) => {
-              if(!item.completed)
-                return <ToDoItem key={index} task={item} onRemove={handleRemove} onCompletion={handleComplete} onSubmitEdit={handleEdit}/>
-            })}
-          </ul>
-        </div>   
+        <ul className='max-w-xl w-full bg-gray-50 rounded-xl shadow-xl p-4 flex flex-col gap-3'>
+          <SearchBar />
+          <ListHeader sortOrder={sortOrder} handleSortOrderChange={handleSortOrderChange} handleFilterBy={handleFilterBy} filterByStatus={filterBy}/>
+          {items.map((item, index) => {
+            if(!item.completed)
+              return <ToDoItem key={index} task={item} onRemove={handleRemove} onCompletion={handleComplete} onSubmitEdit={handleEdit}/>
+          })}
+        </ul>   
         break;
       case 2: fullListRender = 
-        <div className={`flex flex-col items-center justify-center w-full max-w-2xl mx-auto px-4`}>
-          <ul className='max-w-xl w-full bg-gray-50 rounded-xl shadow-xl p-4'>
-            <ListHeader sortOrder={sortOrder} handleSortOrderChange={handleSortOrderChange} handleFilterBy={handleFilterBy} filterByStatus={filterBy}/>
-            {items.map((item, index) => {
-              if(item.completed)
-                return <ToDoItem key={index} task={item} onRemove={handleRemove} onCompletion={handleComplete} onSubmitEdit={handleEdit}/>
-            })}
-          </ul>
-        </div> 
+        <ul className='max-w-xl w-full bg-gray-50 rounded-xl shadow-xl p-4 flex flex-col gap-3'>
+          <SearchBar />
+          <ListHeader sortOrder={sortOrder} handleSortOrderChange={handleSortOrderChange} handleFilterBy={handleFilterBy} filterByStatus={filterBy}/>
+          {items.map((item, index) => {
+            if(item.completed)
+              return <ToDoItem key={index} task={item} onRemove={handleRemove} onCompletion={handleComplete} onSubmitEdit={handleEdit}/>
+          })}
+        </ul>
         break;
     }
   } 
@@ -120,9 +118,11 @@ const Todolist = () => {
   return (
     <div className='grid grid-cols-1 m-2 text-zinc-100 gap-8'>
       <InputHeader handleSubmit={handleSubmit} handleChange={handleChange} inputValue={inputValue}/>
-      {
-       fullListRender 
-      } 
+      <div className={`flex flex-col items-center justify-center w-full max-w-2xl mx-auto px-4`}>
+        {
+        fullListRender 
+        } 
+      </div>
     </div>
   )
 }
